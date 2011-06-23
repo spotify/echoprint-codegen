7ddf2f9eab8aee4aa01a7c6fe54aecb3f891a3ff
Alastair Porter <alastair@echonest.com>
2011-06-23 15:26:27 -0400

    Example using pyechonest to look up a fingerprint from a file

---
+++ examples/lookup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# This script takes an audio file and performs an echoprint lookup on it.
+# Requirements: pyechonest >= 4.2.15 http://code.google.com/p/pyechonest/
+#               The echoprint-codegen binary (run make from ../src)
+#               an Echo Nest API key
+
+import sys
+import os
+
+import pyechonest.config as config
+import pyechonest.song as song
+
+config.CODEGEN_BINARY_OVERRIDE = os.path.abspath("../echoprint-codegen")
+
+# Put your API key in a shell variable ECHO_NEST_API_KEY, or put it here
+# config.ECHO_NEST_API_KEY='KEY HERE'
+
+def lookup(file):
+    # Note that song.identify reads just the first 
+    fp = song.util.codegen(file)
+    if len(fp) and "code" in fp[0]:
+        result = song.identify(query_obj=fp, version="4.11")
+        print "Got result:", result
+        print "Artist: %s (%s)" % (result[0].artist_name, result[0].artist_id)
+        print "Song: %s (%s)" % (result[0].title, result[0].id)
+    else:
+        print "Couldn't decode", file
+            
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print >>sys.stderr, "Usage: %s <audio file>" % sys.argv[0]
+        sys.exit(1)
+    lookup(sys.argv[1])
