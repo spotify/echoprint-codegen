313a5d32648abf379640b2ee84e51901e3d6136d
Andrew Nesbit <anesbit@echoprint.com>
2012-02-22 13:29:23 +0000

    Increased maximum allowed file length.

--- src/Params.h
+++ src/Params.h
@@ -16,7 +16,11 @@ namespace Params {
         const uint Channels = 1;
         const float SecondsPerChunk = 10.0f;
         const uint BytesPerSample = 4; // floats
+#ifdef _LP64
+        const uint MaxSamples = 133000000; // 201 minutes @ 11025 samples per second.
+#else
         const uint MaxSamples = 66977792; // TODO: Remove this, or set it intelligently, at least. Good for 32-bit analyzer.
+#endif
     }
 }
 
