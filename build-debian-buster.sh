8ab93d485e0c2ce6f5f4d7e5cbd75f73ce3cde20
gmega <giuliano.mega@gmail.com>
2021-10-27 10:36:05 -0300

    set JAVA_HOME for make script

--- build-debian-buster.sh
+++ build-debian-buster.sh
@@ -10,6 +10,7 @@ apt install -y build-essential ffmpeg libboost1.67-dev libtag1-dev zlib1g-dev
 # This thing needs JNI headers to be built, even if you're building only the CLI. 
 # Fortunately for us it compiles with the version in Buster.
 apt install -y openjdk-11-jdk
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
 
 cd src
 make
