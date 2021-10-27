858f1f4d0626e22337a54e8dc40ff3834fc2bdf6
gmega <giuliano.mega@gmail.com>
2021-10-27 10:30:06 -0300

    add openjdk to build

---
+++ build-debian-buster.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 
+# Simple recipe for building echoprint-codegen on Debian Buster. Called
+# from our Cloudbuild config.
+set -e
+
+apt update
+apt install -y build-essential ffmpeg libboost1.67-dev libtag1-dev zlib1g-dev 
+
+# This thing needs JNI headers to be built, even if you're building only the CLI. 
+# Fortunately for us it compiles with the version in Buster.
+apt install -y openjdk-11-jdk
+
+cd src
+make
